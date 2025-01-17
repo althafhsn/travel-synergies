d800b0ba6fc5d1dc541538c3d25b79818e6ca304
althafhsn <althafhsn.c@gmail.com>
2025-01-18 04:08:46 +0530

    fi

--- components/button.tsx
+++ components/button.tsx
@@ -1,21 +1,22 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface ButtonProps {
-    type: 'button' | 'submit';
-    title: string;
-    icon?: string;
-    variant: 'btn_dark_green';
-
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant: 'btn_dark_green';
 }
 
-const button = ({ type, title, icon, variant }: ButtonProps) => {
-    return (
-        <button type={type}
-            className={`flexCenter gap-3 rounded-full border ${variant}`}>
-            {icon && <Image src={icon} alt={title} width={24} height={24} />}
-            <label className="bold-16 whitespace-nowrap">{title}</label>
-        </button>
-    )
-}
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <span className="font-bold text-16 whitespace-nowrap">{title}</span>
+    </button>
+  );
+};
 
-export default button
+export default Button;
